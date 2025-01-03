e3b19831f55eec9082ed92ebc66a9cb817015131
Rob Pickerill <r.pickerill@gmail.com>
2025-01-03 13:38:06 +0000

    fix docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 
-# Start Nginx in the background
-nginx -g 'daemon off;' &
+CERT_PATH="/etc/letsencrypt/live/spotplotter.com/fullchain.pem"
+KEY_PATH="/etc/letsencrypt/live/spotplotter.com/privkey.pem"
 
-# Wait for Nginx to start
-sleep 5
+# Check if SSL certificates exist
+if [ ! -f "$CERT_PATH" ] || [ ! -f "$KEY_PATH" ]; then
+    echo "No SSL certificate found. Starting Nginx temporarily without SSL..."
+    nginx -g "daemon off;" &
 
-# Obtain SSL certificate if not already obtained
-certbot certonly --nginx --non-interactive --agree-tos --email rob@spotplotter.com -d yourdomain.com
+    # Wait for Nginx to start
+    sleep 5
 
-# Set up a loop to renew the certificate every 12 hours
+    echo "Obtaining SSL certificate with Certbot..."
+    certbot certonly --nginx --non-interactive --agree-tos --email rob@spotplotter.com -d yourdomain.com
+
+    echo "Certificate obtained. Reloading Nginx with SSL..."
+    nginx -s reload
+fi
+
+# Run Certbot renewal loop every 12 hours
 while true; do
     sleep 12h
-    certbot renew
+    certbot renew --quiet
+    nginx -s reload
 done
